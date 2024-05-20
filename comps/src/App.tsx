d1c4fc9af4bc95154f5231f3e794231dc64bbe3c
Versatilerz <nils.vangestel@gmail.com>
2024-05-20 11:25:18 +0200

    created a ButtonPage

--- comps/src/App.tsx
+++ comps/src/App.tsx
@@ -1,41 +1,9 @@
-import Button from "./components/Button";
-import { GoChevronRight } from "react-icons/go";
-import { GoHubot } from "react-icons/go";
-import { GoGitMerge } from "react-icons/go";
+import ButtonPage from "./pages/ButtonPage";
 
 const App = () => {
-  const clickHandler = () => {
-    console.log("clicked!");
-  };
-
   return (
     <div>
-      <div>
-        <Button primary className="mb-5" onClick={clickHandler}>
-          <GoChevronRight />
-          Click me
-        </Button>
-      </div>
-      <div>
-        <Button succes outline onMouseEnter={clickHandler}>
-          <GoGitMerge />
-          See deal
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <GoHubot />
-          Reserve
-        </Button>
-      </div>
-      <div>
-        <Button secondary>Buy now</Button>
-      </div>
-      <div>
-        <Button warning rounded>
-          Try me
-        </Button>
-      </div>
+      <ButtonPage />
     </div>
   );
 };
---
+++ comps/src/pages/ButtonPage.tsx
@@ -0,0 +1,43 @@
+import Button from "../components/Button";
+import { GoChevronRight } from "react-icons/go";
+import { GoHubot } from "react-icons/go";
+import { GoGitMerge } from "react-icons/go";
+
+const ButtonPage = () => {
+  const clickHandler = () => {
+    console.log("clicked!");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button primary className="mb-5" onClick={clickHandler}>
+          <GoChevronRight />
+          Click me
+        </Button>
+      </div>
+      <div>
+        <Button succes outline onMouseEnter={clickHandler}>
+          <GoGitMerge />
+          See deal
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoHubot />
+          Reserve
+        </Button>
+      </div>
+      <div>
+        <Button secondary>Buy now</Button>
+      </div>
+      <div>
+        <Button warning rounded>
+          Try me
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonPage;
