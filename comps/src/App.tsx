946f8b83d95ea4bc33db87bbfc74dae9dd14c652
Versatilerz <nils.vangestel@gmail.com>
2024-05-20 10:26:11 +0200

    button compoment + styling

--- comps/src/App.tsx
+++ comps/src/App.tsx
@@ -1,5 +1,43 @@
+import Button from "./components/Button";
+import { GoChevronRight } from "react-icons/go";
+import { GoHubot } from "react-icons/go";
+import { GoGitMerge } from "react-icons/go";
+
 const App = () => {
-  return <div></div>;
+  const clickHandler = () => {
+    console.log("clicked!");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button primary className="mb-5" onClick={clickHandler}>
+          <GoChevronRight />
+          Click me
+        </Button>
+      </div>
+      <div>
+        <Button succes outline onMouseEnter={clickHandler}>
+          <GoGitMerge />
+          See deal
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoHubot />
+          Reserve
+        </Button>
+      </div>
+      <div>
+        <Button secondary>Buy now</Button>
+      </div>
+      <div>
+        <Button warning rounded>
+          Try me
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ comps/src/components/Button.tsx
@@ -0,0 +1,67 @@
+import className from "classnames";
+import { twMerge } from "tailwind-merge";
+
+type ButtonProps = {
+  rounded?: Boolean;
+  outline?: Boolean;
+  //   purpose?: string;
+  primary?: Boolean;
+  secondary?: Boolean;
+  succes?: Boolean;
+  warning?: Boolean;
+  danger?: Boolean;
+  children?: React.ReactNode;
+  [key: string]: any;
+};
+
+const Button: React.FC<ButtonProps> = ({
+  rounded,
+  outline,
+  children,
+  primary,
+  secondary,
+  succes,
+  warning,
+  danger,
+  ...rest
+}) => {
+  const validateProps = () => {
+    const props = [primary, secondary, succes, warning, danger];
+    const trueProps = props.filter((prop) => {
+      return prop === true;
+    });
+
+    if (trueProps.length > 1) {
+      throw new Error(
+        "Only one of 'primary', 'secondary', 'success', 'warning', or 'danger' can be set to true."
+      );
+    }
+  };
+
+  validateProps();
+
+  const classes = twMerge(
+    className(rest.className, "flex items-center px-3 py-1.5  border w-40", {
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": succes,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && succes,
+      "text-yellow-400": outline && warning,
+      "text-red-500": danger && outline,
+    })
+  );
+
+  return (
+    <button {...rest} className={classes}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
