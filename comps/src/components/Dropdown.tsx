2bf4aaa40d9035c8eb1040cb8b44079f5d939aa1
Versatilerz <nils.vangestel@gmail.com>
2024-05-22 22:24:09 +0200

    adding css to dropdown

--- comps/src/components/Dropdown.tsx
+++ comps/src/components/Dropdown.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import { GoChevronUp } from "react-icons/go";
 
 export type Options = {
   label: string;
@@ -24,18 +26,31 @@ const Dropdown: React.FC<{
 
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={() => handleOptionClick(option)} key={option.value}>
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
   });
 
-  // const content = selection ? selection : "Select...";
-
   return (
-    <div>
-      <div onClick={handleToggle}>{value?.label ?? "Select..."}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleToggle}
+      >
+        {value?.label ?? "Select..."}
+        {!isOpen && <GoChevronDown className="text-lg" />}
+        {isOpen && <GoChevronUp className="text-lg" />}
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 };
