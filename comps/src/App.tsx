27947ec9a00bbe844513fbb1f528219d94df1825
Versatilerz <nils.vangestel@gmail.com>
2024-05-22 22:08:06 +0200

    Made it so you can select something (also looked at Nullish Coalescing vs  Logical OR

--- comps/src/App.tsx
+++ comps/src/App.tsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
-import Dropdown from "./components/Dropdown";
+import Dropdown, { Options } from "./components/Dropdown";
 
 const App = () => {
-  const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState<Options>({ label: "", value: "" });
   const options = [
     { label: "Red", value: "red" },
     { label: "Green", value: "Green" },
     { label: "Blue", value: "Blue" },
   ];
 
-  const handleSelect = () => {};
+  const handleSelect = (option: Options) => {
+    setSelected(option);
+  };
 
-  return <Dropdown options={options} />;
+  return (
+    <Dropdown options={options} selection={selected} onSelect={handleSelect} />
+  );
 };
 
 export default App;
--- comps/src/components/Dropdown.tsx
+++ comps/src/components/Dropdown.tsx
@@ -6,20 +6,35 @@ export type Options = {
 };
 export type DropdownProps = Options[];
 
-const Dropdown: React.FC<{ options: DropdownProps }> = ({ options }) => {
+const Dropdown: React.FC<{
+  options: DropdownProps;
+  onSelect: (option: Options) => void;
+  selection: Options;
+}> = ({ options, onSelect, selection }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => {
     setIsOpen((currentIsOpen) => !currentIsOpen);
   };
 
+  const handleOptionClick = (option: Options) => {
+    setIsOpen(false);
+    onSelect(option);
+  };
+
   const renderedOptions = options.map((option) => {
-    return <div key={option.value}>{option.label}</div>;
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
   });
+
+  // const content = selection ? selection : "Select...";
 
   return (
     <div>
-      <div onClick={handleToggle}>Select...</div>
+      <div onClick={handleToggle}>{selection?.label || "Select..."}</div>
       {isOpen && <div>{renderedOptions}</div>}
     </div>
   );
