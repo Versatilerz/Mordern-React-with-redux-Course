8811b243641bcfda6a99547eae919db1c859545e
N. vG <nils.vangestel@gmail.com>
2024-05-25 23:28:02 +0200

    add navigation context

---
+++ comps/src/context/navigation.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, createContext, useEffect, useState } from "react";
+
+const NavigationContext = createContext({});
+
+const NavigationProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener("popstate", handler);
+
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
+
+  const navigate = (to: string) => {
+    window.history.pushState({}, "", to);
+    setCurrentPath(to);
+  };
+
+  return (
+    <NavigationContext.Provider
+      value={{
+        currentPath,
+        navigate,
+      }}
+    >
+      {children}
+    </NavigationContext.Provider>
+  );
+};
+
+export { NavigationProvider };
+export default NavigationContext;
