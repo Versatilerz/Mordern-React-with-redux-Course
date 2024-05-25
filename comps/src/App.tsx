a06e845e51272e826c2b2d86fa1cefac10d830d1
N. vG <nils.vangestel@gmail.com>
2024-05-25 22:23:46 +0200

    got prettier up and running again

--- comps/src/App.tsx
+++ comps/src/App.tsx
@@ -1,17 +1,5 @@
-import AccordionPage from "./pages/AccordionPage";
-import ButtonPage from "./pages/ButtonPage";
-import DropDownPage from "./pages/DropdownPage";
-
 const App = () => {
-  return (
-    <div>
-      <AccordionPage />
-      <div className=" my-10 w-2/4 h-1 bg-gray-300"></div>
-      <ButtonPage />
-      <div className="my-10 w-2/4 h-1 bg-gray-300"></div>
-      <DropDownPage />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
