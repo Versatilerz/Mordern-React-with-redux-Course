99ce617663aec5e05efedec168dca617dc385355
N. vG <nils.vangestel@gmail.com>
2024-05-26 22:04:42 +0200

    More modal setup

--- comps/src/pages/ModalPage.tsx
+++ comps/src/pages/ModalPage.tsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
+import Button from "../components/Button";
 import Modal from "../components/Modal";
 
 const ModalPage = () => {
-  return <Modal />;
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const onDismiss = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div>
+      <Button primary onClick={handleClick}>
+        Open Modal
+      </Button>
+      {showModal && <Modal />}
+    </div>
+  );
 };
 export default ModalPage;
