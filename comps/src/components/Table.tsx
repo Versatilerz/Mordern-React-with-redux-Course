c7467e72a7d86a48f13166cc428f5442acb6d032
N. vG <nils.vangestel@gmail.com>
2024-05-31 21:32:11 +0200

    sorting examples added

--- comps/src/components/Table.tsx
+++ comps/src/components/Table.tsx
@@ -46,3 +46,38 @@ const Table: React.FC<{
 };
 
 export default Table;
+
+//data sorting:
+// numbers:
+// data.sort((a, b) => {
+//   return a-b;
+// });
+//
+// strings (alphabatical):
+// data.sort((a,b) => {
+// return a.localeCompare(b);
+// });
+//
+// objects (Transofrm 1 value of the objects and sort on that):
+// Examples:
+// const data = [
+//   { name: "Orange", cost: 1, score: 5 },
+//   { name: "Apple", cost: 5, score: 3 },
+//   { name: "Banana", cost: 13, score: 1 },
+//   { name: "Lime", cost: 2, score: 4 },
+// ];
+
+// const getSortValue = (fruit) => {
+//   return fruit.score;
+// };
+
+// // filter object string or number
+// data.sort((a, b) => {
+//   const valueA = getSortValue(a);
+//   const valueB = getSortValue(b);
+//   if (typeof valueA === "string") {
+//     return valueA.localeCompare(valueB);
+//   } else {
+//     return valueA - valueB;
+//   }
+// });
