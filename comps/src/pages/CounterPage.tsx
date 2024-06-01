589b4b2a23a11345d8fc27ad8b5f2c8d14b2659b
Nils van Gestel <nvangestel@yellowtail.nl>
2024-06-01 15:53:38 +0200

    refactor usestate in counter page (setup for usereducer

--- comps/src/pages/CounterPage.tsx
+++ comps/src/pages/CounterPage.tsx
@@ -1,14 +1,53 @@
+import { ChangeEvent, useState } from "react";
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
+import Panel from "../components/Panel";
+// import useCounter from "../hooks/use-counter";
 
 const CounterPage: React.FC<{ initCount: number }> = ({ initCount }) => {
-  const { counter, increment } = useCounter({ initCount });
+  // const { counter, increment } = useCounter({ initCount });
+  const [counter, setCounter] = useState(initCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value);
+    console.log(value);
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setCounter(counter + valueToAdd);
+    setValueToAdd(0);
+  };
 
   return (
-    <div>
-      <h1>Count is {counter}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {counter}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          value={valueToAdd || ""}
+          onChange={handleChange}
+          className="p-1 m-3 bg-gray-50 border-gray-300"
+        />
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   );
 };
 
