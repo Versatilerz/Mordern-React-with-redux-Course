5d4a4f0881a645e2b1e1252af1309c9fd2ab755c
N. vG <nils.vangestel@gmail.com>
2024-06-02 21:11:08 +0200

    setting up useReducer

--- comps/src/pages/CounterPage.tsx
+++ comps/src/pages/CounterPage.tsx
@@ -1,37 +1,54 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useReducer, useState } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 // import useCounter from "../hooks/use-counter";
 
+type ReducerState = {
+  counter: number;
+  valueToAdd: number;
+};
+
+const reducer = (state: ReducerState) => {
+  return {
+    ...state,
+    counter: state.counter + 1,
+  };
+};
+
 const CounterPage: React.FC<{ initCount: number }> = ({ initCount }) => {
   // const { counter, increment } = useCounter({ initCount });
-  const [counter, setCounter] = useState(initCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [counter, setCounter] = useState(initCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    counter: initCount,
+    valueToAdd: 0,
+  });
 
   const increment = () => {
-    setCounter(counter + 1);
+    // setCounter(counter + 1);
+    dispatch();
   };
 
   const decrement = () => {
-    setCounter(counter - 1);
+    // setCounter(counter - 1);
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const value = parseInt(event.target.value);
     console.log(value);
-    setValueToAdd(value);
+    // setValueToAdd(value);
   };
 
   const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    setCounter(counter + valueToAdd);
-    setValueToAdd(0);
+    // setCounter(counter + valueToAdd);
+    // setValueToAdd(0);
   };
 
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count is {counter}</h1>
+      <h1 className="text-lg">Count is {state.counter}</h1>
       <div className="flex flex-row">
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
@@ -41,7 +58,7 @@ const CounterPage: React.FC<{ initCount: number }> = ({ initCount }) => {
         <label>Add a lot!</label>
         <input
           type="number"
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           className="p-1 m-3 bg-gray-50 border-gray-300"
         />
