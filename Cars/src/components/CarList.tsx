ad8df03a1ecbc7c65762eac64ba292764fe677d9
N. vG <nils.vangestel@gmail.com>
2024-06-09 14:03:28 +0200

    add delete cars + bulma styling

--- Cars/src/components/CarList.tsx
+++ Cars/src/components/CarList.tsx
@@ -1,8 +1,36 @@
-import { useAppSelector } from "../store/hooks";
+import { removeCar } from "../store";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { Car } from "../store/slices/formSlice";
 
 const CarList = () => {
   const { data } = useAppSelector((state) => state.cars);
+  const dispatch = useAppDispatch();
 
-  return <div>CarList</div>;
+  const handleCarDelete = (car: Car) => {
+    dispatch(removeCar(car.id || ""));
+  };
+
+  const renderedCars = data.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          onClick={() => handleCarDelete(car)}
+          className="button is-danger"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 };
 export default CarList;
