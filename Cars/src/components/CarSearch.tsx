414fa2ead0603f9f24556a3cbd02b646ca54feff
N. vG <nils.vangestel@gmail.com>
2024-06-11 22:23:26 +0200

    Added the CarSearch component

--- Cars/src/components/CarSearch.tsx
+++ Cars/src/components/CarSearch.tsx
@@ -1,4 +1,30 @@
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { changeSearchTerm } from "../store";
+import { ChangeEvent } from "react";
+
 const CarSearch = () => {
-  return <div>CarSearch</div>;
+  const dispatch = useAppDispatch();
+  const searchTerm = useAppSelector((state) => state.cars.searchTerm);
+
+  const handleSearchTermChange = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch(changeSearchTerm(event.target.value));
+  };
+
+  return (
+    <div className="list-header">
+      <h3 className="title is-3">
+        <div className="search field is-horizontal">
+          <label className="label" htmlFor="search"></label>
+          <input
+            type="text"
+            id="search"
+            className="input"
+            value={searchTerm}
+            onChange={handleSearchTermChange}
+          />
+        </div>
+      </h3>
+    </div>
+  );
 };
 export default CarSearch;
