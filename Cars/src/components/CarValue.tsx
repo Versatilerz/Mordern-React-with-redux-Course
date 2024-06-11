4dfb6318757254d81f04a5eb56a9167f9da1c0e4
N. vG <nils.vangestel@gmail.com>
2024-06-11 22:43:50 +0200

    adding a calc for totalcost and look for the searchterm to update the calcs.

--- Cars/src/components/CarValue.tsx
+++ Cars/src/components/CarValue.tsx
@@ -1,4 +1,14 @@
+import { useAppSelector } from "../store/hooks";
+
 const CarValue = () => {
-  return <div>CarValue</div>;
+  const totalCost = useAppSelector(({ cars: { data, searchTerm } }) =>
+    data
+      .filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((acc, car) => acc + car.cost, 0)
+  );
+
+  return <div className="car-value">Total Costs: ${totalCost}</div>;
 };
 export default CarValue;
