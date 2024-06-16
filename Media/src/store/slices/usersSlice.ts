e4d3f82930545f19e281da9098beaf6812d626d7
N. vG <nils.vangestel@gmail.com>
2024-06-16 22:25:17 +0200

    setup async thunk fetch + extraReducers concept

--- Media/src/store/slices/usersSlice.ts
+++ Media/src/store/slices/usersSlice.ts
@@ -1,17 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 type Users = {
   data: [];
+  isLoading: boolean;
+  error: null;
 };
 
 const initialState: Users = {
   data: [],
+  isLoading: false,
+  error: null,
 };
 
 const usersSlice = createSlice({
   name: "users",
   initialState: initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
