b20e6441f1dafa7e59c4eece78f5517b6cb3b52d
N. vG <nils.vangestel@gmail.com>
2024-06-16 22:39:56 +0200

    setup the store usdispatch useselector

--- Media/src/store/index.ts
+++ Media/src/store/index.ts
@@ -4,3 +4,7 @@ import { usersReducer } from "./slices/usersSlice";
 export const store = configureStore({
   reducer: { users: usersReducer },
 });
+
+// typescript setup for useDispatch and useSelector
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
