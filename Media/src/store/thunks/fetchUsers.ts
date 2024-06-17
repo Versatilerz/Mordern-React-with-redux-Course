742798f71feba1375f7df2b7ba384b6ee386221b
N. vG <nils.vangestel@gmail.com>
2024-06-17 22:35:10 +0200

    added timer to test loaders

--- Media/src/store/thunks/fetchUsers.ts
+++ Media/src/store/thunks/fetchUsers.ts
@@ -3,5 +3,16 @@ import axios from "axios";
 
 export const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
+
+  // pause is dev only
+  await pause(1000);
+
   return response.data;
 });
+
+// dev only -- remove when going to prod
+const pause = (duration: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
