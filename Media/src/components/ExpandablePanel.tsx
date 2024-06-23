8e0b2b3a6fbeea913fa6429eb590677cb9275792
N. vG <nils.vangestel@gmail.com>
2024-06-23 10:22:09 +0200

    implement chevron arrows and click to open/close panel

--- Media/src/components/ExpandablePanel.tsx
+++ Media/src/components/ExpandablePanel.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 type Props = {
   header: JSX.Element;
@@ -6,14 +7,23 @@ type Props = {
 };
 
 const ExpendablePanel: React.FC<Props> = ({ header, children }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
         <div className="flex flex-row items-center justify-between">
           {header}
         </div>
+        <div onClick={handleClick}>
+          {expanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </div>
       </div>
-      <div className="p-2 border-t">{children}</div>
+      {expanded && <div className="p-2 border-t">{children}</div>}
     </div>
   );
 };
